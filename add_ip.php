0fa7f3a5035a5b515d9b2b14053a8ba9f2251912
vignesh <vigneshp.posnic@gmail.com>
2018-06-06 23:02:54 +0530

    Add Ip UI added

---
+++ add_ip.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Sufee Admin - HTML5 Admin Template</title>
+    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-icon.png">
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/flag-icon.min.css">
+    <link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
+    <!-- <link rel="stylesheet" href="assets/css/bootstrap-select.less"> -->
+    <link rel="stylesheet" href="assets/scss/style.css">
+    <link rel="stylesheet" href="assets/css/lib/chosen/chosen.min.css">
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
+
+</head>
+<body>
+      
+
+    <!-- Right Panel -->
+
+    <div id="right-panel" class="right-panel">
+ <div class="breadcrumbs">
+            <div class="col-sm-4">
+                <div class="page-header float-left">
+                    <div class="page-title">
+                        <h1>Dashboard</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="page-header float-right">
+                    <div class="page-title">
+                        <ol class="breadcrumb text-right">
+                            <li><a href="#">Dashboard</a></li>
+                            <li><a href="#">Forms</a></li>
+                            <li class="active">Advanced</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="content mt-3">
+            <div class="animated fadeIn">
+
+                <div class="row">
+
+                   <div class="col-sm-6 offset-sm-3">
+                        <div class="card">
+                           <div class="card-header">
+                                <strong class="card-title">Add Ip</strong>
+                            </div>
+                            <div class="card-body">
+
+  <div class="card-body card-block">
+                      
+ 
+				   <div class="row form-group">     
+<div class="col-md-3">IP</div>
+<div class="col col-md-9"> <input type="text" id="input1-group2" name="input1-group2" placeholder="Text" class="form-control">
+															  </div>
+				   
+				   </div>
+				   
+				   <div class="row form-group">     
+<div class="col-md-3">Dns Name</div>
+<div class="col col-md-9"> <input type="text" id="input1-group2" name="input1-group2" placeholder="Text" class="form-control">
+															  </div>
+				   
+				   </div>
+
+					  </div>
+                     
+                            </div>
+								<div class="card-footer" align='center'>
+                      
+						  <button type="submit" class="btn btn-success btn-sm">
+                          <i class="fa fa-dot-circle-o"></i> Submit
+                        </button>
+                       
+                      </div>
+                        </div>
+
+                       </div>
+
+  
+                </div>
+
+
+            </div><!-- .animated -->
+        </div><!-- .content -->
+
+
+    </div><!-- /#right-panel -->
+
+    <!-- Right Panel -->
+
+
+    <script src="assets/js/vendor/jquery-2.1.4.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/lib/chosen/chosen.jquery.min.js"></script>
+
+    <script>
+        jQuery(document).ready(function() {
+            jQuery(".standardSelect").chosen({
+                disable_search_threshold: 10,
+                no_results_text: "Oops, nothing found!",
+                width: "100%"
+            });
+        });
+    </script>
+
+</body>
+</html>
